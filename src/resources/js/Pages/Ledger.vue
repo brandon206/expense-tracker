a7029b51b23490df784614979f1ca09493b2215e
brandon206 <brandonpark206@gmail.com>
2021-03-11 00:24:01 -0800

    updating ledger component to fetch from graphql endpoint using vue apollo, updating schema graphql file to create queries for ledger-s, transaction-s, category-s, and creating types for the different models. minor change in routes file

--- src/resources/js/Pages/Ledger.vue
+++ src/resources/js/Pages/Ledger.vue
@@ -3,13 +3,53 @@
         <v-app>
             <v-container fluid>
                 <h1>Ledger Page</h1>
+                <div v-if="$apollo.loading">Loading...</div>
+                <div v-else>
+                    <h2>{{ ledger.name }}</h2>
+                    <div>{{ ledger.description }}</div>
+                    <div v-for="transaction in ledger.transactions" :key="transaction.id">
+                        <div>Updated: {{ transaction.updated_at }}</div>
+                        <div>Created: {{ transaction.created_at }}</div>
+                        <div>{{ transaction.description }}</div>
+                        <div>Amount: {{ transaction.amount }}</div>
+                        <div>Status: {{ transaction.status }}</div>
+                    </div>
+                </div>
             </v-container>
         </v-app>
     </div>
 </template>
 
 <script>
-export default {};
+import gql from 'graphql-tag';
+
+export default {
+    apollo: {
+        ledger: {
+            query: gql`query($id: ID!) {
+                ledger(id: $id) {
+                    id
+                    name
+                    description
+                    transactions
+                    {
+                        id
+                        description
+                        amount
+                        status
+                        created_at
+                        updated_at
+                    }
+                }
+            }`,
+            variables() {
+                return {
+                    id: this.$route.params.id
+                }
+            },
+        }
+    },
+};
 </script>
 
 <style></style>
