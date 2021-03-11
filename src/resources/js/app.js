1ae613675a2d2185ce65215ea50b053ae3c22ef8
brandon206 <brandonpark206@gmail.com>
2021-03-11 00:18:04 -0800

    update app.js to add vue apollo to vue app and update cors.php to allow graphql

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -5,12 +5,30 @@ import VueRouter from 'vue-router';
 import vuetify from './vuetify';
 import router from './Router';
 import App from './App.vue';
+import ApolloClient from 'apollo-boost';
+import VueApollo from 'vue-apollo';
 
 Vue.use(VueRouter);
+Vue.use(VueApollo);
+
+
+const apolloClient = new ApolloClient({
+    // You should use an absolute URL here
+    uri: 'http://127.0.0.1:80/graphql',
+    // handles the CORS error
+    // fetchOptions: {
+    //     mode: 'no-cors',
+    // },
+})
+
+const apolloProvider = new VueApollo({
+    defaultClient: apolloClient,
+})
 
 const app = new Vue({
     el: '#app',
     router,
+    apolloProvider,
     vuetify,
     components: {
         App,
